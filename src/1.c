10015771e54c388b99fac3a9789e7370e3d919b4
moorehelenk56439 <moore_helenk56439@gmx.com>
2025-03-04 20:52:34 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,26 @@
+
+int main() {
+  int a = 5;
+  float b = 3.14;
+  char c = 'c';
+
+  if (a > 0) {
+    printf("The value of a is positive\n");
+  } else {
+    printf("The value of a is not positive\n");
+  }
+
+  if (b > 0) {
+    printf("The value of b is positive\n");
+  } else {
+    printf("The value of b is not positive\n");
+  }
+
+  if (c > 0) {
+    printf("The value of c is positive\n");
+  } else {
+    printf("The value of c is not positive\n");
+  }
+
+  return 0;
+}
